387d0ec3805b777be9a9f4e823ea4d1b47d1f7c2
giorgiobaroncelli <g.baroncelli@gmail.com>
2015-06-12 17:16:19 +0200

    nothing

---
+++ LinkedList/ArrayOfDistinctIntegerInRandomOrderTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Code4Fun
+{
+    public class ArrayOfDistinctIntegerInRandomOrderTest
+    {
+        private Int64 bufferSize = 100L;
+
+        private Int64[] buffer ;
+
+        public ArrayOfDistinctIntegerInRandomOrderTest()
+        {
+            this.buffer =
+              Exercises.GetArrayOfDistinctIntegerInRandomOrder(bufferSize);
+        }
+
+        [Fact]
+        public void ShouldLessThanOneNotExists()
+        {
+           Assert.Equal(1, this.buffer.Min());
+        }
+
+        [Fact]
+        public void ShouldGreaterThanSizeNotExists()
+        {
+            Assert.Equal(bufferSize, this.buffer.Max());
+        }
+
+
+        [Fact]
+        public void ShouldAllDistinct()
+        {
+            Assert.True(this.AllDistinct(this.buffer));
+        }
+
+        private bool AllDistinct(Int64[] buffer)
+        {
+            var orderedBuffer = buffer.OrderBy(b=>b).ToArray();
+
+            for (var i = 0; i < orderedBuffer.Length - 1; i++)
+            {
+                if (orderedBuffer[i] == orderedBuffer[i + 1]) return false;
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ LinkedList/Exercises.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Code4Fun
+{
+    public static class Exercises
+    {
+        public static T GetNthElementFromRightInSinglePass<T>(IEnumerable<T> enumerable, int index)
+        {
+            var ringBuffer = new T[index];
+            
+            var counter = 0;
+            foreach (var element in enumerable)
+            {
+                ringBuffer[counter++ % index] = element;
+            }
+
+            return ringBuffer[counter % index];
+        }
+
+        public static Int64[] GetArrayOfDistinctIntegerInRandomOrder(Int64 size)
+        {
+            var arrayOfDistinctIntegerInRandomOrder = new Int64[size];
+            var arrayOfRandomDouble = new double[size];
+
+            var random = new Random();
+
+            /*    for (var i = 0; i < size;i++ )
+              {
+                  arrayOfDistinctIntegerInRandomOrder[i] = i + 1;
+
+                  arrayOfRandomDouble[i] = random.NextDouble();
+              }
+
+              Array.Sort(arrayOfRandomDouble, arrayOfDistinctIntegerInRandomOrder);
+               */
+
+                var usedBuffer = new bool[size];            
+                var j = 0L;
+
+                for (var i = 0; i < size; i++)
+                {
+                    var r = DateTime.Now.Ticks % size; //random.Next(size);
+                    if (usedBuffer[r])
+                    {
+                        if (j % 2 == 0)
+                        {
+                            r = Array.LastIndexOf(usedBuffer,  false);
+                        }
+                        else
+                        {
+                            r = Array.IndexOf(usedBuffer, false);
+                        }
+
+                        j++;
+                    }
+
+                    usedBuffer[r] = true;
+
+                    arrayOfDistinctIntegerInRandomOrder[i] = r + 1;
+                }
+
+                return arrayOfDistinctIntegerInRandomOrder;
+        }
+    }
+}
